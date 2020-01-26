9de3b6c7e73439d883754856805ec5e3e383d13f
Glenn Gijsberts <glenngijsberts95@gmail.com>
2020-01-26 01:59:26 +0100

    😅 Animated the switch navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch'
+import { Transition } from 'react-native-reanimated'
 
 import AppTabNavigator from './AppTabNavigator'
 import AuthNavigator from './AuthNavigator'
 import LoadingAuthScreen from '../screens/LoadingAuthScreen'
 
 export default createAppContainer(
-  createSwitchNavigator(
+  createAnimatedSwitchNavigator(
     {
       // You could add another route here for authentication.
       // Read more at https://reactnavigation.org/docs/en/auth-flow.html
@@ -19,6 +21,12 @@ export default createAppContainer(
     },
     {
       initialRouteName: 'Loading',
+      transition: (
+        <Transition.Sequence>
+          <Transition.Out type="fade" durationMs={200} />
+          <Transition.In type="fade" durationMs={200} />
+        </Transition.Sequence>
+      ),
     }
   )
 )
