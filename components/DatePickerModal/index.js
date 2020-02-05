fe5aa2daed4987ffabb85af3976f754d89816e19
Glenn Gijsberts <glenngijsberts95@gmail.com>
2020-02-05 16:04:34 +0100

    👐 Implemented date picker component

---
+++ components/DatePickerModal/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import DateTimePickerModal from 'react-native-modal-datetime-picker'
+
+const DatePickerModal = ({
+  show,
+  onConfirm,
+  onCancel,
+  title,
+  confirmLabel,
+  cancelLabel,
+}) => (
+  <DateTimePickerModal
+    isVisible={show}
+    mode="date"
+    onConfirm={onConfirm}
+    onCancel={onCancel}
+    headerTextIOS={title}
+    confirmTextIOS={confirmLabel}
+    cancelTextIOS={cancelLabel}
+  />
+)
+
+DatePickerModal.defaultProps = {
+  confirmLabel: 'Aanpassen',
+  cancelLabel: 'Annuleren',
+}
+
+DatePickerModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  confirmLabel: PropTypes.string,
+  cancelLabel: PropTypes.string,
+}
+
+export default DatePickerModal
